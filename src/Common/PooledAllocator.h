3e39bde718daf28129420b5915d3146e9d1e71c9
janschu99 <105646877+janschu99@users.noreply.github.com>
2024-01-15 14:17:15 +0100

    Clean up PooledAllocator code
    Also adding some comments (according to my understanding of the code)

--- src/Common/PooledAllocator.h
+++ src/Common/PooledAllocator.h
@@ -3,25 +3,24 @@
 
 #include <vector>
 
-//PooledAllocator allocates objects T in fixed-size blocks (specified in the constructor);
-//allocate returns an uninitialized T*;
-//free returns an object to the pool
+//PooledAllocator allocates objects T in fixed-size blocks (specified in the constructor)
 template<typename T>
 class PooledAllocator {
 	public:
-		//Construct with given block size
 		PooledAllocator(size_t blockSize);
 		~PooledAllocator();
-		//Return an uninitialized object
-		T* allocate();
-		//Return an object to the pool
-		void free(T *elementToFree);
+		T* allocate(); //Returns a default-constructed T*
+		void free(T* elementToFree); //Returns an object to the pool
 	private:
+		class Pool;
+		std::vector<Pool*> pools; //list of pools (=blocks)
+		size_t blockSize;
+		int currentPos;
+		std::vector<T*> freeList; //list of freed entries (in all pools combined)
 		class Pool {
 			public:
-				Pool(size_t poolSize) :
-						currentPos(0), poolSize(poolSize) {
-					data = new T[poolSize];
+				Pool(size_t poolSize) : currentPos(0), poolSize(poolSize) {
+					data=new T[poolSize];
 				}
 				~Pool() {
 					delete[] data;
@@ -33,44 +32,41 @@ class PooledAllocator {
 			private:
 				mutable size_t currentPos;
 				size_t poolSize;
-				T *data;
+				T* data;
 		};
-		std::vector<Pool*> pool;
-		size_t blockSize;
-		int currentPos;
-		//The free list
-		std::vector<T*> freeList;
 };
 
 template<typename T>
 PooledAllocator<T>::PooledAllocator(size_t blockSize) :
 		blockSize(blockSize), currentPos(0) {
-	pool.push_back(new Pool(blockSize));
+	pools.push_back(new Pool(blockSize)); //create first pool
 }
 
 template<typename T>
 PooledAllocator<T>::~PooledAllocator() {
-	for (size_t i = 0; i<pool.size(); i++)
-		delete pool[i];
+	for (size_t i = 0; i<pools.size(); i++) //delete all pools
+		delete pools[i];
 }
 
 template<typename T>
 T* PooledAllocator<T>::allocate() {
-	if (freeList.size()>0) {
-		T *lastElement = freeList.back();
+	if (freeList.size()>0) { //try to reuse freed entries (doesn't matter in which pool)
+		T* lastElement = freeList.back();
 		freeList.pop_back();
 		return lastElement;
 	}
-	T *p = pool[currentPos]->allocate();
-	if (p) return p;
-	pool.push_back(new Pool(blockSize));
+	//no more free entries, allocate a new one in the current pool
+	T* p = pools[currentPos]->allocate();
+	if (p!=NULL) return p; //allocation successful, return
+	//allocation failed since current pool is full, create new pool and allocate there
+	pools.push_back(new Pool(blockSize));
 	currentPos++;
-	return pool.back()->allocate();
+	return pools.back()->allocate();
 }
 
 template<typename T>
-void PooledAllocator<T>::free(T *elementToFree) {
-	freeList.push_back(elementToFree);
+void PooledAllocator<T>::free(T* elementToFree) {
+	freeList.push_back(elementToFree); //add entry to freeList, to be reused later
 }
 
 #endif
